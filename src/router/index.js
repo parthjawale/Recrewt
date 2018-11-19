d9febeac6c21c5237119875b641a5e2d2b471403
parthjawale <parth.169105122@muj.manipal.edu>
2018-11-19 13:00:59 +0530

    main page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import MainPage from "@/components/MainPage";
 import Auth from "./auth";
 import Dashboard from "./dashboard";
 import Profile from "./profile";
@@ -9,6 +10,16 @@ import Jobs from "./jobs";
 Vue.use(Router);
 
 export default new Router({
-  routes: [...Auth, ...Dashboard, ...Profile, ...Jobs],
+  routes: [
+    {
+      path: "/",
+      component: MainPage,
+      name: "Home"
+    },
+    ...Auth,
+    ...Dashboard,
+    ...Profile,
+    ...Jobs
+  ],
   mode: "history"
 });
